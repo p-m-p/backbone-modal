9b42860b1a8bf60c8c7df2e3d0ae5e0aaae08822
Phil Parsons <phil@profilepicture.co.uk>
2012-10-13 08:50:17 +0100

    Add more tests

--- spec/test-bb-modal.js
+++ spec/test-bb-modal.js
@@ -1,6 +1,7 @@
 ;(function () {
 
   var env = jasmine.getEnv()
+    , $window = $(window)
     , report = new jasmine.HtmlReporter
     , view;
 
@@ -12,14 +13,48 @@
     render: function () { this.$el.html(this.template()); return this; }
   }));
 
-  describe('Create modal and add to DOM', function () {
-    var modal = new Backbone.Modal();
+  describe('Modal life cycle', function () {
+    var modal = new Backbone.Modal;
 
-    it('should create the modal', function () {
+    it('should create the Modal', function () {
+      var $modal = $('.bb-modal-overlay');
+
+      // check the overlay
+      expect($modal.length).toBe(1);
+      expect($modal.width()).toEqual($window.width());
+      expect($modal.height()).toEqual($window.height());
+
+      // check the modal contaimer
+      expect(modal.$('> .bb-modal').length).toBe(1);
+      expect(modal.$('> .bb-modal > .bb-modal-content').length).toBe(1);
+      expect(modal.$('> .bb-modal > .bb-modal-close').length).toBe(1);
+      expect(modal.$('> .bb-modal > .bb-modal-close').text()).toBe('x');
+      expect(modal.$el.is(':hidden')).toBe(true);
+    });
+
+    it('should open the Modal to display a view', function () {
       modal.open(view.render());
+
+      // check the modal is displayed and the view is added
       expect(modal.$el.is(':visible')).toBe(true);
       expect(modal.$('p').text()).toEqual('Hello world!');
     });
+
+    it('should close the Modal', function () {
+      modal.close();
+
+      expect(modal.$el.is(':hidden')).toBe(true);
+    });
+
+    it('should destroy the Modal', function () {
+      modal.destroy();
+
+      expect($('.bb-modal-overlay').length).toBe(0);
+    });
+  });
+
+  describe('Modal Hooks', function () {
+    
   });
 
   env.execute();
